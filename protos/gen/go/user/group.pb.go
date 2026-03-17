a559df33414fec101f12c43a87e989080a07ac09
Kai120789 <zoomer120789@gmail.com>
2026-03-18 01:51:58 +0300

    fix: return tutor to update group response and delete subject id from request

--- protos/gen/go/user/group.pb.go
+++ protos/gen/go/user/group.pb.go
@@ -262,9 +262,8 @@ type UpdateGroupRequest struct {
 	Id            int64                  `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
 	Title         string                 `protobuf:"bytes,2,opt,name=title,proto3" json:"title,omitempty"`
 	Description   string                 `protobuf:"bytes,3,opt,name=description,proto3" json:"description,omitempty"`
-	SubjectId     int64                  `protobuf:"varint,4,opt,name=subject_id,json=subjectId,proto3" json:"subject_id,omitempty"`
-	TgGroupLink   *string                `protobuf:"bytes,5,opt,name=tg_group_link,json=tgGroupLink,proto3,oneof" json:"tg_group_link,omitempty"`
-	TgChatId      *int64                 `protobuf:"varint,6,opt,name=tg_chat_id,json=tgChatId,proto3,oneof" json:"tg_chat_id,omitempty"`
+	TgGroupLink   *string                `protobuf:"bytes,4,opt,name=tg_group_link,json=tgGroupLink,proto3,oneof" json:"tg_group_link,omitempty"`
+	TgChatId      *int64                 `protobuf:"varint,5,opt,name=tg_chat_id,json=tgChatId,proto3,oneof" json:"tg_chat_id,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -320,13 +319,6 @@ func (x *UpdateGroupRequest) GetDescription() string {
 	return ""
 }
 
-func (x *UpdateGroupRequest) GetSubjectId() int64 {
-	if x != nil {
-		return x.SubjectId
-	}
-	return 0
-}
-
 func (x *UpdateGroupRequest) GetTgGroupLink() string {
 	if x != nil && x.TgGroupLink != nil {
 		return *x.TgGroupLink
@@ -347,8 +339,9 @@ type UpdateGroupResponse struct {
 	Title         string                 `protobuf:"bytes,2,opt,name=title,proto3" json:"title,omitempty"`
 	Description   string                 `protobuf:"bytes,3,opt,name=description,proto3" json:"description,omitempty"`
 	SubjectId     int64                  `protobuf:"varint,4,opt,name=subject_id,json=subjectId,proto3" json:"subject_id,omitempty"`
-	TgGroupLink   *string                `protobuf:"bytes,5,opt,name=tg_group_link,json=tgGroupLink,proto3,oneof" json:"tg_group_link,omitempty"`
-	TgChatId      *int64                 `protobuf:"varint,6,opt,name=tg_chat_id,json=tgChatId,proto3,oneof" json:"tg_chat_id,omitempty"`
+	TutorId       int64                  `protobuf:"varint,5,opt,name=tutor_id,json=tutorId,proto3" json:"tutor_id,omitempty"`
+	TgGroupLink   *string                `protobuf:"bytes,6,opt,name=tg_group_link,json=tgGroupLink,proto3,oneof" json:"tg_group_link,omitempty"`
+	TgChatId      *int64                 `protobuf:"varint,7,opt,name=tg_chat_id,json=tgChatId,proto3,oneof" json:"tg_chat_id,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -407,6 +400,13 @@ func (x *UpdateGroupResponse) GetDescription() string {
 func (x *UpdateGroupResponse) GetSubjectId() int64 {
 	if x != nil {
 		return x.SubjectId
+	}
+	return 0
+}
+
+func (x *UpdateGroupResponse) GetTutorId() int64 {
+	if x != nil {
+		return x.TutorId
 	}
 	return 0
 }
@@ -1208,27 +1208,26 @@ const file_user_group_proto_rawDesc = "" +
 	"\n" +
 	"tg_chat_id\x18\a \x01(\x03H\x01R\btgChatId\x88\x01\x01B\x10\n" +
 	"\x0e_tg_group_linkB\r\n" +
-	"\v_tg_chat_id\"\xe8\x01\n" +
+	"\v_tg_chat_id\"\xc9\x01\n" +
 	"\x12UpdateGroupRequest\x12\x0e\n" +
 	"\x02id\x18\x01 \x01(\x03R\x02id\x12\x14\n" +
 	"\x05title\x18\x02 \x01(\tR\x05title\x12 \n" +
-	"\vdescription\x18\x03 \x01(\tR\vdescription\x12\x1d\n" +
+	"\vdescription\x18\x03 \x01(\tR\vdescription\x12'\n" +
+	"\rtg_group_link\x18\x04 \x01(\tH\x00R\vtgGroupLink\x88\x01\x01\x12!\n" +
 	"\n" +
-	"subject_id\x18\x04 \x01(\x03R\tsubjectId\x12'\n" +
-	"\rtg_group_link\x18\x05 \x01(\tH\x00R\vtgGroupLink\x88\x01\x01\x12!\n" +
-	"\n" +
-	"tg_chat_id\x18\x06 \x01(\x03H\x01R\btgChatId\x88\x01\x01B\x10\n" +
+	"tg_chat_id\x18\x05 \x01(\x03H\x01R\btgChatId\x88\x01\x01B\x10\n" +
 	"\x0e_tg_group_linkB\r\n" +
-	"\v_tg_chat_id\"\xe9\x01\n" +
+	"\v_tg_chat_id\"\x84\x02\n" +
 	"\x13UpdateGroupResponse\x12\x0e\n" +
 	"\x02id\x18\x01 \x01(\x03R\x02id\x12\x14\n" +
 	"\x05title\x18\x02 \x01(\tR\x05title\x12 \n" +
 	"\vdescription\x18\x03 \x01(\tR\vdescription\x12\x1d\n" +
 	"\n" +
-	"subject_id\x18\x04 \x01(\x03R\tsubjectId\x12'\n" +
-	"\rtg_group_link\x18\x05 \x01(\tH\x00R\vtgGroupLink\x88\x01\x01\x12!\n" +
+	"subject_id\x18\x04 \x01(\x03R\tsubjectId\x12\x19\n" +
+	"\btutor_id\x18\x05 \x01(\x03R\atutorId\x12'\n" +
+	"\rtg_group_link\x18\x06 \x01(\tH\x00R\vtgGroupLink\x88\x01\x01\x12!\n" +
 	"\n" +
-	"tg_chat_id\x18\x06 \x01(\x03H\x01R\btgChatId\x88\x01\x01B\x10\n" +
+	"tg_chat_id\x18\a \x01(\x03H\x01R\btgChatId\x88\x01\x01B\x10\n" +
 	"\x0e_tg_group_linkB\r\n" +
 	"\v_tg_chat_id\"$\n" +
 	"\x12RemoveGroupRequest\x12\x0e\n" +
